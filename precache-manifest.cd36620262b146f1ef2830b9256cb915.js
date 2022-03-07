4333f7cb4f4d940b9508a33fc2b2fec762c43fa7
Oscar Rubio <rubio.sevilla.oscar@gmail.com>
2022-03-07 16:50:45 -0600

    deploy

---
+++ precache-manifest.cd36620262b146f1ef2830b9256cb915.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f90954234566e6e2506e",
+    "url": "/vue-router-3/css/app.9e981aa5.css"
+  },
+  {
+    "revision": "b7177151994fb78fbb8d",
+    "url": "/vue-router-3/css/chunk-vendors.52eaf30c.css"
+  },
+  {
+    "revision": "4bbb59a43eac63fade11",
+    "url": "/vue-router-3/css/customers.5edc9ccd.css"
+  },
+  {
+    "revision": "d096c0d511574bb41a18ed61deb5a06e",
+    "url": "/vue-router-3/img/flag_lang_cn.d096c0d5.png"
+  },
+  {
+    "revision": "209843a904a0a0561fc8f9d0a7d69f66",
+    "url": "/vue-router-3/img/flag_lang_en.209843a9.png"
+  },
+  {
+    "revision": "842830c634c0f42c35eca38d13f676d9",
+    "url": "/vue-router-3/img/flag_lang_es.842830c6.png"
+  },
+  {
+    "revision": "33e0b1c0c12c6f759cb44f74941bf9cc",
+    "url": "/vue-router-3/img/search_no_result.33e0b1c0.png"
+  },
+  {
+    "revision": "7dc91d65b7257cbcc8cde8b70c4e01dc",
+    "url": "/vue-router-3/img/searching.7dc91d65.png"
+  },
+  {
+    "revision": "c3349addcff3d10fb3284f6882d2238e",
+    "url": "/vue-router-3/img/spinner.c3349add.gif"
+  },
+  {
+    "revision": "eb539c30f064dec60320e27e14efed1c",
+    "url": "/vue-router-3/index.html"
+  },
+  {
+    "revision": "f90954234566e6e2506e",
+    "url": "/vue-router-3/js/app.3d8db0b3.js"
+  },
+  {
+    "revision": "b7177151994fb78fbb8d",
+    "url": "/vue-router-3/js/chunk-vendors.283201f0.js"
+  },
+  {
+    "revision": "4bbb59a43eac63fade11",
+    "url": "/vue-router-3/js/customers.c3c6bce8.js"
+  },
+  {
+    "revision": "bb74e5d1aa03c59d7366534884e243c1",
+    "url": "/vue-router-3/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-router-3/robots.txt"
+  }
+]);
